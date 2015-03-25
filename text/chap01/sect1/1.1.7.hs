7b6dcc6e0bf6f1aa088243f709fec057051a902d
lesguillemets <lesguillemets@users.noreply.github.com>
2015-03-25 22:18:03 +0900

    update 1.1.7

--- text/chap01/sect1/1.1.7.hs
+++ text/chap01/sect1/1.1.7.hs
@@ -1,8 +1,17 @@
 module NewtonMethod where
 
+-- | sqrt by Newton's method.
+-- >>> sqrtIter 1 2
+-- 1.4142156862745097
+--
+-- prop> abs ((sqrtIter n)^2 - n) < 0.01
+
 sqrtIter :: Double -> Double -> Double
 sqrtIter guess x = if goodEnough guess x then guess
                                          else sqrtIter (improv guess x) x
+
+threshold :: Double
+threshold = 0.00001
 
 improv :: Double -> Double -> Double
 improv guess x = avarage guess (x/guess)
@@ -11,7 +20,9 @@ avarage :: Double -> Double -> Double
 avarage = ((/ 2) .) . (+)
 
 goodEnough :: Double -> Double -> Bool
-goodEnough guess x = abs (x - guess^2) < 0.0001
+goodEnough guess x = abs (x - guess^2) < threshold
 
 main = do
     print $ sqrtIter 1 2
+    print $ sqrtIter 1 3
+    print $ sqrtIter 1 320
