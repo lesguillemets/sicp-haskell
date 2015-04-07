bdc5d6ddc935c067ca3958983ebea79ed979f8cb
lesguillemets <lesguillemets@users.noreply.github.com>
2015-04-07 16:03:02 +0900

    fix the definition of list

--- text/chap02/sect2/2.2.2.hs
+++ text/chap02/sect2/2.2.2.hs
@@ -4,8 +4,8 @@ data TList a = Nil | Cons (TList a) (TList a) | V a deriving (Show)
 -- We should probably doing this with Cons [TList a] | V a
 -- Note that his allows lists that does not terminate with Nil, like
 -- Cons (V 2) (V 1)
-list :: [a] -> TList a
-list = foldr (Cons . V) Nil
+list :: [TList a] -> TList a
+list = foldr Cons Nil
 
 car :: TList a -> TList a
 car (Cons p _) = p
@@ -21,5 +21,5 @@ countLeaves (V _) = 1
 countLeaves (Cons p q) = countLeaves p + countLeaves q
 
 -- |
--- >>> countLeaves $ Cons (list [1,2]) (list [3,4])
+-- >>> countLeaves $ Cons (list . map V $ [1,2]) (list . map V $ [3,4])
 -- 4
