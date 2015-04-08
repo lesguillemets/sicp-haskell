ebd4c8ee5005f0715bf08cd707f06c2b3f6ff5da
lesguillemets <lesguillemets@users.noreply.github.com>
2015-04-08 13:55:03 +0900

    ex 2.29
    workaround, but working fine

--- exercise/chap02/sect2/2.29.hs
+++ exercise/chap02/sect2/2.29.hs
@@ -19,6 +19,7 @@ makeBranch len structure = list [V len, structure]
 
 totalWeight :: TList Int -> Int
 totalWeight Nil = 0
+totalWeight (V _) = 0
 totalWeight (Cons (V _) (Cons (V weight) Nil)) = weight
 totalWeight mob = totalWeight (rightBranch $ mob)
                     + totalWeight (leftBranch $ mob)
@@ -31,16 +32,14 @@ isBalanced x = totalWeight (rightBranch x) == totalWeight (leftBranch x)
 -- >>> let lev1 = makeMobile (makeBranch 5 lev0) (makeBranch 4 (V 4))
 -- >>> let leftHand = makeBranch 20 (V 19)
 -- >>> let rightHand = makeBranch 15 lev1
--- >>> let lev3 = makeMobile leftHand rightHand
---
-main = do
-    let a =  makeMobile (makeBranch 3 (V 5)) (makeBranch 2 (V 10))
-    print $ a
-    print $ totalWeight a
-    let b =  makeBranch 3 (V 5)
-    print $ b
-    let c =  makeMobile b b
-    print $ c
-    let d = makeMobile (makeBranch 4 c) (makeBranch 5 c)
-    print $ d
-    print . totalWeight $ d
+-- >>> let lev2 = makeMobile leftHand rightHand
+-- >>> totalWeight lev0
+-- 15
+-- >>> totalWeight lev1
+-- 19
+-- >>> isBalanced lev1
+-- False
+-- >>> totalWeight lev2
+-- 38
+-- >>> isBalanced lev2
+-- True
