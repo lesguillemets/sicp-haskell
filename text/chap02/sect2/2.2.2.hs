c683fad34947aa828d0c3d85a7c874358dd9c3e2
lesguillemets <lesguillemets@users.noreply.github.com>
2015-04-08 15:36:58 +0900

    text/2.2.2

--- text/chap02/sect2/2.2.2.hs
+++ text/chap02/sect2/2.2.2.hs
@@ -26,3 +26,17 @@ countLeaves (Cons p q) = countLeaves p + countLeaves q
 -- |
 -- >>> countLeaves $ Cons (list . map V $ [1,2]) (list . map V $ [3,4])
 -- 4
+
+scaleTree :: (Num a) => TList a -> a -> TList a
+scaleTree tree factor = case tree of
+                            Nil -> Nil
+                            (V x) -> V (x*factor)
+                            (Cons  pre post)
+                                -> Cons (scaleTree pre factor)
+                                        (scaleTree post factor)
+-- |
+-- >>> let l = list [V 1, list [V 2, list [V 3, V 4], V 5] , list [V 6, V 7]]
+-- >>> l
+-- Cons (V 1) (Cons (Cons (V 2) (Cons (Cons (V 3) (Cons (V 4) Nil)) (Cons (V 5) Nil))) (Cons (Cons (V 6) (Cons (V 7) Nil)) Nil))
+-- >>> scaleTree l 10
+-- Cons (V 10) (Cons (Cons (V 20) (Cons (Cons (V 30) (Cons (V 40) Nil)) (Cons (V 50) Nil))) (Cons (Cons (V 60) (Cons (V 70) Nil)) Nil))
