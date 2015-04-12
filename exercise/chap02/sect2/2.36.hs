06c39f1a3c394b78432090bfbc268478830d5ac4
lesguillemets <lesguillemets@users.noreply.github.com>
2015-04-13 00:05:55 +0900

    more haskellish

--- exercise/chap02/sect2/2.36.hs
+++ exercise/chap02/sect2/2.36.hs
@@ -1,4 +1,5 @@
 module TwoThirtysix where
+import Data.List (transpose)
 
 -- sensing more generalisations
 accMap :: (a -> b -> b) -> b -> [[a]] -> [b]
@@ -8,6 +9,13 @@ accMap f ini seqs = if null (head seqs)
                             foldr (f . head) ini seqs :
                                 accMap f ini (map tail seqs)
 
+-- Of course we can use this...
+accMap' :: (a -> b -> b) -> b -> [[a]] -> [b]
+accMap' f ini = map (foldr f ini) . transpose
+
 -- |
 -- >>> accMap (+) 0 [[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
+-- [22,26,30]
+--
+-- >>> accMap' (+) 0 [[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
 -- [22,26,30]
