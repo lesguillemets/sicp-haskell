85f6a5a298e212152eca12cf569132d5152489a0
lesguillemets <lesguillemets@users.noreply.github.com>
2015-04-13 00:00:12 +0900

    ex 2.36

---
+++ exercise/chap02/sect2/2.36.hs
@@ -0,0 +1,13 @@
+module TwoThirtysix where
+
+-- sensing more generalisations
+accMap :: (a -> b -> b) -> b -> [[a]] -> [b]
+accMap f ini seqs = if null (head seqs)
+                        then []
+                        else
+                            foldr (f . head) ini seqs :
+                                accMap f ini (map tail seqs)
+
+-- |
+-- >>> accMap (+) 0 [[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
+-- [22,26,30]
