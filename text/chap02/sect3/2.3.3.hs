65a760cdb97f22a14b22b7fce6e6e9fc512be631
lesguillemets <lesguillemets@users.noreply.github.com>
2015-06-06 15:40:48 +0900

    2.3.3.hs

--- text/chap02/sect3/2.3.3.hs
+++ text/chap02/sect3/2.3.3.hs
@@ -1,8 +1,12 @@
 module ExSets where
 
-newtype LSet a = LSet [a]
+newtype LSet a = LSet [a] deriving (Show)
 cons :: a -> LSet a -> LSet a
 cons x (LSet xs) = LSet (x:xs)
+
+fromList :: (Eq a) => [a] -> LSet a
+fromList [] = LSet []
+fromList (x:xs) = adjoinSet x (fromList xs)
 
 elemOfSet :: (Eq a) => a -> LSet a -> Bool
 elemOfSet _ (LSet []) = False
@@ -19,3 +23,8 @@ intersectionSet (LSet (x:xs)) s1 =
         ( if x `elemOfSet` s1
             then cons x
             else id ) $ intersectionSet (LSet xs) s1
+
+unionSet :: (Eq a) => LSet a -> LSet a -> LSet a
+unionSet (LSet []) s = s
+unionSet s (LSet []) = s
+unionSet (LSet (x:xs)) s = adjoinSet x (unionSet (LSet xs) s)
