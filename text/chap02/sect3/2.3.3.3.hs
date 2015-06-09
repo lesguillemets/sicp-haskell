8f6860977b7f4f6487dec6d7b821595fabf01e8f
lesguillemets <lesguillemets@users.noreply.github.com>
2015-06-09 17:41:14 +0900

    2.3.3.3

---
+++ text/chap02/sect3/2.3.3.3.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE RankNTypes #-}
+module BTSet (Tree, elemOfSet, adjoinSet) where
+
+data Tree a = Tree { entry :: a, lBranch :: Tree a, rBranch :: Tree a }
+            | EmptyTree
+instance Show a => Show (Tree a) where
+    show EmptyTree = "."
+    show (Tree e l r) = concat [ show e, "(",
+                                show l, ",", show r, ")" ]
+
+type BTSet a = Ord a => Tree a
+
+elemOfSet :: Ord a => a -> Tree a -> Bool
+elemOfSet _ EmptyTree = False
+elemOfSet x (Tree e l r) =
+        case x `compare`  e of
+            EQ -> True
+            LT -> elemOfSet x l
+            GT -> elemOfSet x r
+
+adjoinSet :: Ord a => a -> Tree a -> Tree a
+adjoinSet x EmptyTree = Tree x EmptyTree EmptyTree
+adjoinSet x t@(Tree e l r) =
+        case x `compare` e of
+            EQ -> t
+            LT -> Tree e (adjoinSet x l) r
+            GT -> Tree e l (adjoinSet x r)
+-- |
+-- >>> import Data.List
+-- >>> mapM_ print $ scanl (flip adjoinSet) EmptyTree [3,4,2,8,2]
+-- .
+-- 3(.,.)
+-- 3(.,4(.,.))
+-- 3(2(.,.),4(.,.))
+-- 3(2(.,.),4(.,8(.,.)))
+-- 3(2(.,.),4(.,8(.,.)))
