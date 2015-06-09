5a03a0f9f8eef714dd7767beef75a70fcdd96d9c
lesguillemets <lesguillemets@users.noreply.github.com>
2015-06-09 16:09:03 +0900

    ex 2.61-2.62

--- text/chap02/sect3/2.3.3.2.hs
+++ text/chap02/sect3/2.3.3.2.hs
@@ -1,5 +1,9 @@
 module OLSet where
-newtype OLSet a = OLSet [a]
+-- $setup
+-- >>> let a = OLSet ([1,2,3,6,7,8] :: [Int])
+-- >>> let b = OLSet ([1,2,5,6,10,12] :: [Int])
+
+newtype OLSet a = OLSet [a] deriving (Show, Eq)
 -- well, Data.Vector may be more suitable
 
 cons :: a -> OLSet a -> OLSet a
@@ -14,8 +18,13 @@ elemOfSet _ (OLSet [])  = False
 elemOfSet e (OLSet (x:xs)) =
     case e `compare` x of
         EQ -> True
-        GT -> False
-        LT -> elemOfSet e (OLSet xs)
+        LT -> False
+        GT -> elemOfSet e (OLSet xs)
+-- |
+-- >>> elemOfSet 1 a
+-- True
+-- >>> elemOfSet 10 a
+-- False
 
 intersectionSet :: Ord a => OLSet a -> OLSet a -> OLSet a
 intersectionSet (OLSet []) _ = OLSet []
@@ -25,3 +34,42 @@ intersectionSet s0@(OLSet (x0:xs0)) s1@(OLSet (x1:xs1)) =
         EQ -> cons x1 $ intersectionSet (OLSet xs0) (OLSet xs1)
         LT -> intersectionSet (OLSet xs0) s1
         GT -> intersectionSet s0 (OLSet xs1)
+-- |
+-- >>> intersectionSet a b
+-- OLSet [1,2,6]
+
+-- 2.61
+adjoinSet :: Ord a => a -> OLSet a -> OLSet a
+adjoinSet x (OLSet []) = OLSet [x]
+adjoinSet x (OLSet sl) = OLSet $ iter x sl
+    where
+        iter n [] = [n]
+        iter n s@(e:es) =
+            case n `compare` e of
+                EQ -> s
+                LT -> n:s
+                GT -> e: iter n es
+-- |
+-- >>> adjoinSet 4 a
+-- OLSet [1,2,3,4,6,7,8]
+--
+-- >>> adjoinSet 2 a
+-- OLSet [1,2,3,6,7,8]
+--
+-- >>> adjoinSet 10 a
+-- OLSet [1,2,3,6,7,8,10]
+
+-- 2.62
+unionSet :: (Ord a) => OLSet a -> OLSet a -> OLSet a
+unionSet (OLSet s0) (OLSet s1) = OLSet $ union s0 s1
+    where
+        union [] ys = ys
+        union xs [] = xs
+        union l0@(x:xs) l1@(y:ys) =
+            case x `compare` y of
+                EQ -> x : union xs ys
+                GT -> y : union l0 ys
+                LT -> x : union xs l1
+-- |
+-- >>> unionSet a b
+-- OLSet [1,2,3,5,6,7,8,10,12]
