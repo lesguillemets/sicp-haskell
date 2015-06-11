4605bfd9f4927af36c52f8c09f3e86572960746d
lesguillemets <lesguillemets@users.noreply.github.com>
2015-06-11 12:55:28 +0900

    ex 2.65 (1)

--- exercise/chap02/sect3/2.65.hs
+++ exercise/chap02/sect3/2.65.hs
@@ -4,14 +4,17 @@ import qualified Data.Foldable as F
 import Data.Monoid
 
 main = do
-    let a = foldl (flip adjoinSet) EmptyTree [3,4,2,7,5]
-    let b = foldl (flip adjoinSet) EmptyTree [8,5,4,27,15]
-    print $ toList b
+    let a = foldl (flip adjoinSet) EmptyTree ([3,4,2,7,5] :: [Int])
+    let b = foldl (flip adjoinSet) EmptyTree ([8,5,4,27,15] :: [Int])
+    print . toList $ unionSets a b
 
 instance Foldable Tree where
     foldMap f EmptyTree = mempty
     foldMap f (Tree e l r) = foldMap f l `mappend` f e
                                          `mappend` foldMap f r
+
+unionSets :: Ord a => Tree a -> Tree a -> Tree a
+unionSets t0 t1 = fromOrderedList $ joinOrdered (toList t0) (toList t1)
 
 fromOrderedList :: Ord a => [a] -> Tree a
 fromOrderedList es = fst $ partialTree es (length es)
@@ -28,3 +31,12 @@ partialTree elms n =
 
 toList :: Tree a -> [a]
 toList = F.foldr (:) []
+
+joinOrdered :: Ord a => [a] -> [a] -> [a]
+joinOrdered [] xs = xs
+joinOrdered xs [] = xs
+joinOrdered l0@(x:xs) l1@(y:ys) =
+        case x `compare` y of
+            EQ -> y:joinOrdered xs ys
+            GT -> y:joinOrdered l0 ys
+            LT -> x:joinOrdered xs l1
