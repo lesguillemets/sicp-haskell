d328ba905541769627d75e4ca4a7e6e22a2d87ab
lesguillemets <lesguillemets@users.noreply.github.com>
2015-06-11 18:39:39 +0900

    ex 2.65

--- exercise/chap02/sect3/2.65.hs
+++ exercise/chap02/sect3/2.65.hs
@@ -9,7 +9,7 @@ main = do
     print . toList $ unionSets a b
 
 instance Foldable Tree where
-    foldMap f EmptyTree = mempty
+    foldMap _ EmptyTree = mempty
     foldMap f (Tree e l r) = foldMap f l `mappend` f e
                                          `mappend` foldMap f r
 
@@ -40,3 +40,12 @@ joinOrdered l0@(x:xs) l1@(y:ys) =
             EQ -> y:joinOrdered xs ys
             GT -> y:joinOrdered l0 ys
             LT -> x:joinOrdered xs l1
+
+intersectOrderd :: Ord a => [a] -> [a] -> [a]
+intersectOrderd [] _ = []
+intersectOrderd _ []  = []
+intersectOrderd l0@(x:xs) l1@(y:ys) =
+        case x `compare` y of
+            EQ -> x:intersectOrderd xs ys
+            GT -> intersectOrderd l0 ys
+            LT -> intersectOrderd xs l1
