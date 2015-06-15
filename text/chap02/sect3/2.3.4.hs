3de0167df4e384229309b9b7ddbd5d3911905c14
lesguillemets <lesguillemets@users.noreply.github.com>
2015-06-15 12:50:05 +0900

    text / 2.3.4
    "representing Huffman trees"

--- text/chap02/sect3/2.3.4.hs
+++ text/chap02/sect3/2.3.4.hs
@@ -20,3 +20,17 @@ frequencies xs = runST $ do
 -- |
 -- prop> (M.toAscList . frequencies) xs == (map (liftM2 (,) head length) . group . sort) (xs :: [Char])
 
+data HTree a = Leaf { _symbol :: a, _weight :: Int}
+             | Tree {
+                    _left :: HTree a, _right :: HTree a,
+                    _symbols :: [a], _weight :: Int
+             } deriving (Show)
+
+symbols :: HTree a -> [a]
+symbols (Leaf f _) = return f
+symbols (Tree _ _ s _) = s
+
+mkTree :: HTree a -> HTree a -> HTree a
+mkTree t0 t1 = Tree t0 t1 sys wei where
+    sys = symbols t0 ++ symbols t1
+    wei = _weight t0 + _weight t1
