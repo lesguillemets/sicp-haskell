7307ba2709ba1c4ea8c320b62ee6b5bcca497af7
lesguillemets <lesguillemets@users.noreply.github.com>
2015-06-15 13:04:38 +0900

    decoding procedure

--- text/chap02/sect3/2.3.4.hs
+++ text/chap02/sect3/2.3.4.hs
@@ -34,3 +34,23 @@ mkTree :: HTree a -> HTree a -> HTree a
 mkTree t0 t1 = Tree t0 t1 sys wei where
     sys = symbols t0 ++ symbols t1
     wei = _weight t0 + _weight t1
+
+--
+type Bit = Int
+decode :: [Bit] -> HTree a -> [a]
+decode [] _ = []
+decode bits tree =
+    let
+        decode1 [] _ = []
+        decode1 (b:bs) cBranch =
+            case chooseBranch b tree of
+                (Leaf symb _) ->  symb:decode1 bs tree
+                t@(Tree {}) ->  decode1 bs t
+    in
+        decode1 bits tree
+
+
+chooseBranch :: Bit -> HTree a -> HTree a
+chooseBranch 0 = _left
+chooseBranch 1 = _right
+chooseBranch _ = error "invalid bit"
